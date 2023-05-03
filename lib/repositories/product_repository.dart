db6bc4df62ba870a98fabb09cb972e445b8e7bbe
Fabricio Dourado <fcvieira2@gmail.com>
2023-05-02 21:40:31 -0300

    feat: Adicionar metodo findByName

---
+++ lib/repositories/product_repository.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import '../models/courses_model.dart';
+
+class ProductRepository {
+  Future<CoursesModel> findByName(String name) async {
+    var response =
+        await http.get(Uri.parse('http://localhost:8080/products?name=$name'));
+
+    if (response.statusCode != 200) {
+      throw Exception();
+    }
+
+    final responseData = jsonDecode(response.body);
+
+    if (responseData.isEmpty) {
+      throw Exception('Produto não encontrado');
+    }
+
+    return CoursesModel.fromMap(responseData.first);
+  }
+}
