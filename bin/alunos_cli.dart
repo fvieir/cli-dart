d63d3871d104adcc3ff1dea40edeb52582dd075e
Fabricio Dourado <fcvieira2@gmail.com>
2023-05-16 06:42:21 -0300

    chore: Adicionar arquivos csv para fazer leitura de dados

--- bin/alunos_cli.dart
+++ bin/alunos_cli.dart
@@ -2,7 +2,7 @@ import 'package:alunos_cli/src/commands/students/students_commands.dart';
 import 'package:args/command_runner.dart';
 
 void main(List<String> args) {
-  CommandRunner('Exemplo CLI', 'Description CLI')
+  CommandRunner('CLI', 'Find Students and products')
     ..addCommand(StudentsCommands())
     ..run(args);
 }
